6eff0dc4010b539a61f1a54f31c2de57bf2abed7
AngieXiong <363068948@qq.com>
2017-05-09 14:56:16 +0800

    update directory

--- theta-framework/src/main/java/com/theta/framework/controller/HelloWorld.java
+++ theta-framework/src/main/java/com/theta/framework/controller/HelloWorld.java
@@ -3,15 +3,17 @@ package com.theta.framework.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorld {
 
 	@RequestMapping("/hello")
 	@ResponseBody
-	public String hello(){
-		//ModelAndView mv = new ModelAndView("login/login");
+	public ModelAndView hello(){
+		ModelAndView mv = new ModelAndView("login/login");
 		System.out.println("...");
-		return "hello, world! It comes from spring-mvc!";
+		//return "hello, world! It comes from spring-mvc!";
+		return mv;
 	}
 }
