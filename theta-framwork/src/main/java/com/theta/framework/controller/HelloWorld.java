17149ccd5ef7a780b522fad6c6e2e3944a561944
AngieXiong <363068948@qq.com>
2017-05-09 14:17:31 +0800

    change directory name

--- theta-framwork/src/main/java/com/theta/framework/controller/HelloWorld.java
+++ theta-framwork/src/main/java/com/theta/framework/controller/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.theta.framwork.core;
+package com.theta.framework.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,7 @@ public class HelloWorld {
 	@RequestMapping("/hello")
 	@ResponseBody
 	public String hello(){
+		//ModelAndView mv = new ModelAndView("login/login");
 		System.out.println("...");
 		return "hello, world! It comes from spring-mvc!";
 	}
