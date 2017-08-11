d38e597f5086bf0fb898f0b66ce16da051ac47bf
AngieXiong <363068948@qq.com>
2017-08-11 17:58:01 +0800

    add new module:theta-platform

---
+++ theta-platform/src/main/java/org/theta/platform/App.java
@@ -0,0 +1,13 @@
+package org.theta.platform;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
